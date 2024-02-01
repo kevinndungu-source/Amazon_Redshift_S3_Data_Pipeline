8942619c117242b868a90c897ddbf0478dc5c6e6
root <root@DESKTOP-AFGMV1T>
2024-02-01 15:11:18 +0300

    Initial commit

---
+++ Create_Table_SQL_Code.sql
@@ -0,0 +1,20 @@
+CREATE TABLE users (
+	userid INTEGER NOT NULL,
+	username CHAR(8),
+	firstname VARCHAR(30),
+	lastname VARCHAR(30),
+	city VARCHAR(30),
+	state CHAR(2),
+	email VARCHAR(100),
+	phone CHAR(14),
+	likesports BOOLEAN,
+	liketheatre BOOLEAN,
+	likeconcerts BOOLEAN,
+	likejazz BOOLEAN,
+	likeclassical BOOLEAN,
+	likeopera BOOLEAN,
+	likerock BOOLEAN,
+	likevegas BOOLEAN,
+	likebroadway BOOLEAN,
+	likemusicals BOOLEAN
+);
